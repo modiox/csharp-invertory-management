2fe44a4159391433e3e22c947b75a82949289be7
modiox <moodyalyalak@gmail.com>
2024-03-24 13:31:09 +0300

    Initial Pull Request

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System; 
+
+public class Program { 
+    public class Item { 
+        public string? Name { get; }
+        public int Quantity { get; set; }
+
+        private DateTime DateCreated { get; set; }
+
+        public Item(string name, int quantity, DateTime dateCreated) { 
+            if(Quantity < 0){ 
+                Console.WriteLine("Quantity cannot be negative");
+            }
+            Name= name; 
+            Quantity = quantity; 
+            DateCreated = dateCreated; 
+
+        }
+
+    }
+
+}
