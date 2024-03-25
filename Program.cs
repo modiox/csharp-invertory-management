1136782bb7694d1d1129f9b668b271f945a35cc8
modiox <moodyalyalak@gmail.com>
2024-03-25 17:36:48 +0300

    Tested the SortbyNameAsc

--- Program.cs
+++ Program.cs
@@ -113,16 +113,38 @@ public class Program {
      public static void Main(string [] args) { 
         var waterBottle = new Item ("Water Bottle", 10, new DateTime (2023, 1, 1));
         var waterBottle2 = new Item ("Water Gallon", 14, new DateTime (2023, 1, 1));
+        var chocolateBar = new Item("Chocolate Bar", 15, new DateTime(2023, 2, 1));
+        var notebook = new Item("Notebook", 5, new DateTime(2023, 3, 1));
+        var pen = new Item("Pen", 20, new DateTime(2023, 4, 1));
+        var tissuePack = new Item("Tissue Pack", 30, new DateTime(2023, 5, 1));
+        var chipsBag = new Item("Chips Bag", 25, new DateTime(2023, 6, 1));
+        var sodaCan = new Item("Soda Can", 8, new DateTime(2023, 7, 1));
+        var soap = new Item("Soap", 12, new DateTime(2023, 8, 1));
+        var shampoo = new Item("Shampoo", 40, new DateTime(2023, 9, 1));
+        var toothbrush = new Item("Toothbrush", 50, new DateTime(2023, 10, 1));
+        var coffee = new Item("Coffee", 20);
+        var sandwich = new Item("Sandwich", 15);
+        var batteries = new Item("Batteries", 10);
+        var umbrella = new Item("Umbrella", 5);
+        var sunscreen = new Item("Sunscreen", 8);
         // Console.WriteLine($"Item {waterBottle}");
         var store = new Store();
         store.AddItem(waterBottle);
         store.AddItem(waterBottle2);
+        store.AddItem(coffee);
+        store.AddItem(pen);
        // store.DeleteItem("Water Gallon");
         store.PrintItems();
        var volume= store.GetCurrentVolume();
         Console.WriteLine($"The voulme is == {volume}");
-        store.FindItemByName("Water Gallon");
+       // store.FindItemByName("Water Gallon");
+        var collections =store.SortByNameAsc();
+        foreach(var item in collections){  
+            Console.WriteLine($"Sorted ASC: {item}");
 
+
+        }
+       
 
         
        
