89e1792a9d1fa131d36d2392f16b2fbedae9d905
modiox <moodyalyalak@gmail.com>
2024-03-26 20:54:19 +0300

    Separated the files (Store.cs & Item.cs)

---
+++ Item.cs
@@ -0,0 +1,40 @@
+using System; 
+
+namespace  StoreManagement {
+
+    public class Item
+{
+    public string? Name { get; }
+    private int quantity;
+    public int Quantity
+    {
+        get { return quantity; }
+        set { quantity = value; }
+    }
+    public DateTime CreatedDate { get; }
+
+    private DateTime DateCreated { get; set; }
+    //Constructor parameter 
+    public Item(string name, int quantity, DateTime dateCreated = default)
+    {
+        if (quantity < 0)
+        {
+            Console.WriteLine("Quantity cannot be negative");
+        }
+        Name = name;
+        Quantity = quantity;
+        CreatedDate = dateCreated != default ? dateCreated : DateTime.Now;
+
+
+    }
+
+    //To define how the object itmes are represented
+    public override string ToString()
+    {
+        return $"Item Name: {Name}, Item Quanitity: {Quantity}, Date Created: {DateCreated}";
+    }
+
+}
+
+}
+
--- Store.cs
+++ Store.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
+namespace StoreManagement;
 public enum SortOrder
 {
     ASC,
@@ -9,39 +10,7 @@ public enum SortOrder
 }
 public class Program
 {
-    public class Item
-    {
-        public string? Name { get; }
-        private int quantity;
-        public int Quantity
-        {
-            get { return quantity; }
-            set { quantity = value; }
-        }
-        public DateTime CreatedDate { get; }
-
-        private DateTime DateCreated { get; set; }
-        //Constructor parameter 
-        public Item(string name, int quantity, DateTime dateCreated = default)
-        {
-            if (quantity < 0)
-            {
-                Console.WriteLine("Quantity cannot be negative");
-            }
-            Name = name;
-            Quantity = quantity;
-            CreatedDate = dateCreated != default ? dateCreated : DateTime.Now;
-
-
-        }
-
-        //To define how the object itmes are represented
-        public override string ToString()
-        {
-            return $"Item Name: {Name}, Item Quanitity: {Quantity}, Date Created: {DateCreated}";
-        }
-
-    }
+    
 
     public class Store
     {
